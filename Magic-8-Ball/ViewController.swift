912bd09b60bfeb07d2cd469d54539ce50f147139
Dev Karan Singh <devkaransingh856@gmail.com>
2025-09-14 14:51:07 +0530

    Initial Commit

---
+++ Magic-8-Ball/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Magic-8-Ball
+//
+//  Created by Dev Karan Singh on 14/09/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
